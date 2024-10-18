1ff85f9360a00483e869b387d7c94c316f9f7745
alaeddinerami <alaeddinerami7@gmail.com>
2024-10-18 17:19:33 +0100

    Feat: stayling table for film'

--- frontend/src/Components/dashboard/admin-components/Table/TableFilm.jsx
+++ frontend/src/Components/dashboard/admin-components/Table/TableFilm.jsx
@@ -41,7 +41,7 @@ export default function TableFilm() {
       </table>
 
       {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+        <div className="fixed inset-0 flex w-full items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="bg-white p-6 rounded shadow-lg w-1/3">
             <h2 className="text-2xl mb-4">Edit User</h2>
             <form>
