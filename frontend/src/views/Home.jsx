a2b91d5a4c6c9f08ec3f08ff450e73295e073d29
alaeddinerami <alaeddinerami7@gmail.com>
2024-10-22 16:47:55 +0100

    fix: fixing fetch image from minio

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,80 +1,76 @@
-import React, { useState, useEffect } from "react"; 
+import React, { useState, useEffect } from "react";
 import Header from "../Components/header/Header";
-import Card from "../Components/card/card"; 
+import Card from "../Components/card/card";
 import Filter from "../Components/filter/Filter";
-import SeanceCarousel from "../Components/seanceCarousel/SeanceCarousel"; 
-import axios from "axios"; 
+import SeanceCarousel from "../Components/seanceCarousel/SeanceCarousel";
+import axios from "axios";
+import axiosInstance from "../api/axios";
 
 const Home = () => {
   const [films, setFilms] = useState([]);
-  const [seances, setSeances] = useState([]); 
-  const [error, setError] = useState(null); 
-  const [genres, setGenres] = useState([]); 
-  const [selectedGenre, setSelectedGenre] = useState(""); 
+  const [seances, setSeances] = useState([]);
+  const [error, setError] = useState(null);
+  const [genres, setGenres] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState("");
 
   useEffect(() => {
     const fetchFilms = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/api/films", {
+        const response = await axiosInstance.get("films/", {
           headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`, 
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
         });
         console.log(response.data);
-        
-        setFilms(response.data);
-        
 
+        setFilms(response.data);
       } catch (error) {
         console.error("Error fetching films:", error);
-        setError("Failed to fetch films."); 
+        setError("Failed to fetch films.");
       }
     };
 
-    fetchFilms(); 
-  }, []); 
+    fetchFilms();
+  }, []);
 
   const handleFilterChange = (genre) => {
     setSelectedGenre(genre);
   };
   const filteredFilms = selectedGenre
-  ? films.filter(film => film.genre === selectedGenre)
-  : films;
-  
+    ? films.filter((film) => film.genre === selectedGenre)
+    : films;
+
   // console.log(filteredFilms);
   return (
     <div className="bg-black">
       <Header />
       <main className="p-5">
-       
         <div>
           <Filter genres={genres} onFilterChange={handleFilterChange} />
         </div>
 
-       
         <section className="p-5">
           <h2 className="text-white text-2xl font-bold mb-4">Films</h2>
-          {error && <p className="text-red-500">{error}</p>} 
+          {error && <p className="text-red-500">{error}</p>}
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {filteredFilms.map((film) => (
-            
-            <Card
-            key={film._id} 
-            imgSrc={film.image}
-            title={film.title}
-            genre={film.genre}
-            id={film._id}
-            />
-            
-          ))}
+            {filteredFilms.map(
+              (film) => (
+                console.log(`${import.meta.env.VITE_MINIO_URL}${film.image}`),
+                (
+                  <Card
+                    key={film._id}
+                    imgSrc={`${import.meta.env.VITE_MINIO_URL}${film.image}`}
+                    title={film.title}
+                    genre={film.genre}
+                    id={film._id}
+                  />
+                )
+              )
+            )}
           </div>
         </section>
 
         {/* Seances Section */}
-        <section className="p-5">
-          <h2 className="text-white text-2xl font-bold mb-4">Seances</h2>
-          <SeanceCarousel seances={seances} />
-        </section>
       </main>
     </div>
   );
